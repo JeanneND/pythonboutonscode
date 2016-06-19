e83221dd67163615e20cfbc3ad76ae0e8a54e42b
Pierre Nouaille-Degorce <pierre@nativefrenchspeech.com>
2016-06-19 11:24:14 +0200

    Add speed

--- jeushooter.py/jeu final shooter.py
+++ jeushooter.py/jeu final shooter.py
@@ -4,6 +4,18 @@ import time
 tk = Tk()
 canvas = Canvas(tk, width=300, height=500)
 canvas.pack()
+
+# Game speed
+v = 1
+# Les positions du lanceur
+global x1
+global y1
+global x2
+global y2
+# L'identifiant de chaque cube
+global id_cube1
+global id_cube2
+global id_cube3
 global cube1_bouge
 cube1_bouge = False
 global cube2_bouge
@@ -14,57 +26,45 @@ cube3_bouge = False
 def creer_lanceur():
      global id_lanceur
      id_lanceur = canvas.create_rectangle(150, 450, 200, 500, fill='gray')
-     global x1
      x1 = 150
-     global y1
      y1 = 450
-     global x2
      x2 = 200
-     global y2
      y2 = 500
      canvas.bind_all('<KeyPress-w>', creer_balle)
      canvas.bind_all('<KeyPress-a>', lanceur_gauche)
      canvas.bind_all('<KeyPress-d>', lanceur_droite)
 
+def deplacement_lanceur(distance):
+     x1 += distance
+     x2 += distance
+     canvas.move(id_lanceur, distance, 0)
+
 def lanceur_gauche(event):
-     global x1
-     x1 = x1 - 50
-     global x2
-     x2 = x2 - 50
-     canvas.move(id_lanceur, -50, 0)
-
-
+     deplacement_lanceur(-50)
 
 def lanceur_droite(event):
-     global x1
-     x1 = x1 + 50
-     global x2
-     x2 = x2 + 50
-     canvas.move(id_lanceur, 50, 0)
+     deplacement_lanceur(50)
 
 def creer_cubes():
-     global id_cube1
      id_cube1 = canvas.create_rectangle(0, 0, 100, 100, fill='red')
-     global id_cube2
      id_cube2 = canvas.create_rectangle(100, 0, 200, 100, fill='green')
-     global id_cube3
      id_cube3 = canvas.create_rectangle(200, 0, 300, 100, fill='blue')
 
 def deplacement_cube(cube):
      for x in range(0, 30):
          canvas.move(cube, 2, 0)
          tk.update()
-         time.sleep(0.01)
+         time.sleep(0.01 * v)
          canvas.move(cube, -4, 0)
          tk.update()
-         time.sleep(0.01)
+         time.sleep(0.01 * v)
          canvas.move(cube, 2, 0)
          tk.update()
-         time.sleep(0.01)
+         time.sleep(0.01 * v)
      for x in range(0, 50):
          canvas.move(cube, 0, -2)
          tk.update()
-         time.sleep(0.002)
+         time.sleep(0.002 * v)
 
 def bouger_cube1():
      deplacement_cube(id_cube1)
@@ -78,14 +78,12 @@ def bouger_cube3():
      deplacement_cube(id_cube3)
      cube3_bouge = True
 
-creer_cubes()
-
 def creer_balle(event):
      id_balle = canvas.create_oval(x1, y1, x2, y2, fill='#ffd889')
      for x in range(0, 200):
          canvas.move(id_balle, 0, -2)
          tk.update()
-         time.sleep(0.005)
+         time.sleep(0.005 * v)
      if x1 == 0 and not cube1_bouge: bouger_cube1()
      if x1 == 50 and cube1_bouge == False:
          bouger_cube1()
@@ -100,6 +98,8 @@ def creer_balle(event):
      for x in range(0, 50):
          canvas.move(id_balle, 0, -2)
          tk.update()
-         time.sleep(0.005) 
+         time.sleep(0.005 * v) 
 
+# Initialisation
+creer_cubes()
 creer_lanceur()
